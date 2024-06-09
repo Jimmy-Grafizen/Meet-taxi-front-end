c4034c03b3be1aac353e254459ede623c27b05c6
Jimmy-Grafizen <135313298+Jimmy-Grafizen@users.noreply.github.com>
2024-06-09 16:26:01 +0530

    ...

--- src/website/pages/about/AboutUs.jsx
+++ src/website/pages/about/AboutUs.jsx
@@ -8,7 +8,7 @@ export default function AboutUs() {
                 <Header />
                 <div className="w-[75%] mx-auto">
                     <div className="">
-                        <p className='text-[14px] '>Welcome To YBGO</p>
+                        <p className='text-[20px] font-bold'>Welcome To YBGO</p>
                         <p>Welcome to YBGO Cab Rental One of the oldest car hire companies with the youngest fleet of cars in West India. YBGO allows you to hire a rental car of your choice at an affordable rate. Book yourself a car with a chauffeur conveniently through online booking or on your smartphone. To make your travel memorable, rewarding & hassle-free, we offer high-quality service and a host of benefits to heighten your experience. </p>
                         <p>We offer the best car rental service for the tourists who wish to make their holidays comfortable and unforgettable. We always make the effort to bring much more satisfaction, our clients plan out. You will get well maintained (brand new) cars and polite and responsible drivers with excellent communication skills to make your experience pleasant and smooth.</p>
                         <p>YBGO Car rental provides you with the best road trip experience. We offer hourly, daily, weekly, and monthly plans to fulfil your business travel needs, weekends getaway needs, and leisure travel needs.</p>
